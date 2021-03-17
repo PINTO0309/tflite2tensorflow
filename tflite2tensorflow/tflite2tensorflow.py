Bug fixes for FLOOR, TANH, and SOFTMAX

tflite2tensorflow/tflite2tensorflow.py
@@ -938,7 +938,7 @@ def make_graph(ops,
                 input_detail = interpreter._get_tensor_details(op['inputs'][0])
                 input_tensor1 = interpreter.get_tensor(input_detail['index'])
             output_detail = interpreter._get_tensor_details(op['outputs'][0])
-            output_tensor = tf.math.floor(input_tensor, name=get_op_name(output_detail['name']))
+            output_tensor = tf.math.floor(input_tensor1, name=get_op_name(output_detail['name']))
             tensors[output_detail['index']] = output_tensor
 
         elif op_type == 'TANH':
@@ -949,7 +949,7 @@ def make_graph(ops,
                 input_detail = interpreter._get_tensor_details(op['inputs'][0])
                 input_tensor1 = interpreter.get_tensor(input_detail['index'])
             output_detail = interpreter._get_tensor_details(op['outputs'][0])
-            output_tensor = tf.math.tanh(input_tensor, name=get_op_name(output_detail['name']))
+            output_tensor = tf.math.tanh(input_tensor1, name=get_op_name(output_detail['name']))
             tensors[output_detail['index']] = output_tensor
 
         elif op_type == 'DIV':
@@ -1068,7 +1068,7 @@ def make_graph(ops,
             # options = op['builtin_options']
             # beta = int(options['beta'])
             output_detail = interpreter._get_tensor_details(op['outputs'][0])
-            output_tensor = tf.nn.softmax(input_tensor, name=get_op_name(output_detail['name']))
+            output_tensor = tf.nn.softmax(input_tensor1, name=get_op_name(output_detail['name']))
             tensors[output_detail['index']] = output_tensor
 
         elif op_type == 'STRIDED_SLICE':